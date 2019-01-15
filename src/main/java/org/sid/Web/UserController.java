ea49b4541180886b37f5077938c54fb830f1905d
kumohira <naitsaid.youssef@gmail.com>
2019-01-15 23:59:29 +0100

    Minor Change : @PostMapping("/register")

--- src/main/java/org/sid/Web/UserController.java
+++ src/main/java/org/sid/Web/UserController.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.sid.Entities.AppUser;
 import org.sid.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class UserController {
         this.accountService = accountService;
     }
 
+    @PostMapping("/register")
     public AppUser register(@RequestBody UserForm userForm) {
         return accountService.saveUser(userForm.getUsername(), userForm.getPassword(), userForm.getConfirmedPassword());
     }
